bb3fad56f5edb32950c90a3184dad0a6c2cdf6c4
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-12-14 08:42:12 +0900

    REACT NATIVE SETUP

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require('./assets/logo.png'),
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png',
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
